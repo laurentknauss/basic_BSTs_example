808c00dc8e66c5feda10b8be200e3f605475c90a
Laurent Knauss <laurentknauss@protonmail.com>
2023-09-12 12:24:39 +0200

    Create bst_with_integer_test.go

---
+++ test/bst_with_integer_test.go
@@ -0,0 +1,26 @@
+
+
+package main
+
+import (
+	"testing"
+)
+
+func TestInsertAndSearch(t *Testing.T) { 
+	tree := &Node{Key:100}
+	tree.Insert(200)
+	tree.Insert(50)
+
+	if !tree.Search(100) { 
+		t.Errorf("Expected to find 100 in the tree")
+	}
+	if !tree.Search(200) { 
+		t.Errorf("Expected to find 200 in the tree")
+	}
+	if !tree.Search(50) { 
+		t.Errorf("Expected to find 50 in the tree")
+	}
+	if tree.Search(999) { 
+		t.Errorf("Did not expect to find 999 in the tree")
+	}
+}
